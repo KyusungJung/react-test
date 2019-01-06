26f0590e9e4c1e339c0ecfa6359eeb612dcb0bb4
elle0715 <elle0715@gmail.com>
2019-01-06 16:26:30 +0900

    Updates

---
+++ precache-manifest.ff7c78e3ee0a01c600c6c0372e610cac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "8069e049d6303916ece4",
+    "url": "/react-test/static/js/main.8069e049.chunk.js"
+  },
+  {
+    "revision": "1d37698636ef8a3d008e",
+    "url": "/react-test/static/js/1.1d376986.chunk.js"
+  },
+  {
+    "revision": "2802d31f78950238089f00c95b08f990",
+    "url": "/react-test/index.html"
+  }
+];
